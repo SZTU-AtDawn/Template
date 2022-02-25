8f735e81193f36b6c69c6d25187dcce58959198b
Rainiar <49141130+SamuNatsu@users.noreply.github.com>
2022-02-25 19:17:41 +0800

    Update Union_Find.cpp

--- Data structure/Union_Find.cpp
+++ Data structure/Union_Find.cpp
@@ -1,67 +1,84 @@
+// int N -> 最大独立集数量
+
+// 注：log*(n)为迭代对数函数，增长极其缓慢，对应复杂度表达式近似为常数，定义如下：
+//      log*(n) = 0             , n <= 1
+//      log*(n) = log*(log n)    , n > 1
+// 例：log*(2^65536) = 5
+
 // 一般并查集：
-// int fa[] -> 父节点指针
-
-// 注：log*为迭代对数函数，增长极其缓慢，对应复杂度表达式近似为常数
-// log*(n) = 0, n <= 1
-// log*(n) = log*(logn), n > 1
-// log*(2^65536) = 5
-
-// 查找父亲 O(log*n)
-int find(int x) {
-    return fa[x] == x ? x : (fa[x] = find(fa[x]));
-}
-// 检查是否在同一集合
-inline bool judge(int x, int y) {
-    return find(x) == find(y);
-}
-// 合并
-inline void merge(int x, int y) {
-    // 优先合并到y
-    fa[find(x)] = find(y);
-    // 同理：优先合并到x
-    // fa[find(y)] = find(x);
-    // 同理：优先合并到下标较小
-    // x = find(x);
-    // y = find(y);
-    // x < y ? (fa[y] = x) : (fa[x] = y);
-    // 三种可以任选
-}
+struct UnionFind {
+    // 父节点指针
+    int fa[N];
+    // 初始化
+    void init() {
+        for (int i = 1; i <= N; ++i)
+            fa[i] = i;
+    }
+    // 查找父亲 O(log*n)
+    int find(int x) {
+        return fa[x] == x ? x : (fa[x] = find(fa[x]));
+    }
+    // 检查是否在同一集合
+    bool judge(int x, int y) {
+        return find(x) == find(y);
+    }
+    // 合并
+    void merge(int x, int y) {
+        // 优先合并到y
+        fa[find(x)] = find(y);
+        // 同理：优先合并到x
+        // fa[find(y)] = find(x);
+        // 同理：优先合并到下标较小
+        // x = find(x);
+        // y = find(y);
+        // x < y ? (fa[y] = x) : (fa[x] = y);
+        // 三种可以任选
+    }
+};
 
 // 带权并查集：
-// int fa[] -> 父节点指针
-// int len[] -> 当前节点到根的距离
-// int siz[] -> 当前节点所属子树大小
-
-// 查找父亲 O(log*n)
-int find(int x) {
-    if (fa[x] == x)
-        return x;
-    int tmp = fa[x];
-    // 路径压缩
-    fa[x] = find(fa[x]);
-    // 更新节点距离
-    len[x] += len[tmp];
-    // 更新子树大小
-    siz[x] = siz[fa[x]];
-    return fa[x];
-}
-// 检查是否在同一集合
-inline bool judge(int x, int y) {
-    return find(x) == find(y);
-}
-// 合并（具有方向性）
-void merge(int x, int y) {
-    x = find(x);
-    y = find(y);
-    // 将x子树接到y后面
-    fa[x] = y;
-    // 更新节点距离
-    len[x] += siz[y];
-    // 更新子树大小
-    siz[x] = siz[y] += siz[x];
-    // 同理，将y子树接到x后面，只需要交换x和y即可
-}
-// 两节点间的距离
-inline int distance(int x, int y) {
-    return judge(x, y) ? abs(len[x] - len[y]) : -1;
-}
+struct WeightedUnionFind {
+    // 父节点指针、当前节点到根的距离、当前节点所属子树大小
+    int fa[N], len[N], siz[N];
+    // 初始化
+    void init() {
+        for (int i = 1; i <= N; ++i) {
+            fa[i] = i;
+            len[i] = 0;
+            siz[i] = 1;
+        }
+    }
+    // 查找父亲 O(log*n)
+    int find(int x) {
+        if (fa[x] == x)
+            return x;
+        int tmp = fa[x];
+        // 路径压缩
+        fa[x] = find(fa[x]);
+        // 更新节点距离
+        len[x] += len[tmp];
+        // 更新子树大小
+        siz[x] = siz[fa[x]];
+        return fa[x];
+    }
+    // 检查是否在同一集合
+    bool judge(int x, int y) {
+        return find(x) == find(y);
+    }
+    // 合并（具有方向性）
+    void merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        // 将x子树接到y后面
+        fa[x] = y;
+        // 更新节点距离
+        len[x] += siz[y];
+        // 更新子树大小
+        siz[x] = siz[y] += siz[x];
+        // 同理，将y子树接到x后面，只需要交换x和y即可
+    }
+    // 两节点间的距离
+    int distance(int x, int y) {
+        return judge(x, y) ? abs(len[x] - len[y]) : -1;
+    }
+};
