3b5296c75bd584858d1966e197c6c2ace040267e
Rainiar <49141130+SamuNatsu@users.noreply.github.com>
2022-03-13 09:20:10 +0800

    Create Sequence_Automaton.cpp

---
+++ String/Sequence_Automaton.cpp
@@ -0,0 +1,23 @@
+// int N -> 序列长度
+// int next[i][j] -> 自动机结构，从第i个位置开始，下一个字符j的位置
+
+// 序列自动机：对于一个序列，查询其是否存在某一个子序列
+
+// 生成自动机 O(n)
+void build(const char s[]) {
+    for (int i = N - 1; i >= 0; --i) {
+        for (int j = 0; j < 26; ++j)
+            next[i][j] = next[i + 1][j];
+        next[i][s[i] - 'a'] = i;
+    }
+}
+
+// 子序列查询 O(n)
+bool search(const char t[]) {
+    for (int i = 0, j = -1; t[i]; ++i) {
+        j = next[j + 1][t[i] - 'a'];
+        if (j == -1)
+            return false;
+    }
+    return true;
+}
