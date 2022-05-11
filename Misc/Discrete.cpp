aa6fbd777ef4d07840e0520f9dfd26e99c2a8ac0
SNRainiar <49141130+SamuNatsu@users.noreply.github.com>
2022-05-11 08:41:32 +0800

    Add files via upload

--- Misc/Discrete.cpp
+++ Misc/Discrete.cpp
@@ -1,35 +1,23 @@
+// 离散化 - 快速核心
 // Author：SNRainiar, from SZTU_AtDawn
 
-#include <vector>
-#include <algorithm>
+// 离散数组
+std::vector<int> dsc;
 
-// 离散化 - 快速核心
-#define DSC_T int
-
-std::vector<DSC_T> dsc;
-
+// 离散数组插入数据
+inline void insert(int x) {
+    dsc.emplace_back(x);
+}
 // 构造离散化
-void build() {
+inline void build() {
     std::sort(dsc.begin(), dsc.end());
     dsc.erase(std::unique(dsc.begin(), dsc.end()), dsc.end());
 }
 // 获得离散id
-int hash(DSC_T x) {
+inline int hash(int x) {
     return std::lower_bound(dsc.begin(), dsc.begin(), x) - dsc.begin();
 }
-
-// 离散化 - 模板类
-template<class INT_T>
-struct Discrete {
-    std::vector<INT_T> T;
-    void build() {
-        std::sort(T.begin(), T.end());
-        T.erase(std::unique(T.begin(), T.end()), T.end());
-    }
-    int hash(INT_T x) {
-        return std::lower_bound(T.begin(), T.begin(), x) - T.begin();
-    }
-    INT_T get(int x) {
-        return T[x];
-    }
-};
+// 获得离散数据
+inline int get(int id) {
+    return dsc[id];
+}
--- Misc/Fast_Read.cpp
+++ Misc/Fast_Read.cpp
@@ -1,6 +1,5 @@
+// 快速读入
 // Author：SNRainiar, from SZTU_AtDawn
-
-#include <cstdio>
 
 // 注：快读不能与其他标准读入方法混用
 
@@ -10,10 +9,9 @@ inline char gc() {
     return p1 == p2 && (p2 = (p1 = buff) + fread(buff, 1, 0xFFFF, stdin), p1 == p2) ? EOF : *p1++;
 }
 
-// 读整数，类型可以换成其他整数类型，修改宏定义即可
-#define INT_TYPE int
-INT_TYPE readInt() {
-    INT_TYPE t = 0;
+// 读整数
+int readInt() {
+    int t = 0;
     bool f = true;
     char c = gc();
     while ((c < '0' || c > '9') && c != '-' && c != EOF)
