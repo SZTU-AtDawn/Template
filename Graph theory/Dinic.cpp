0bf4581801385716bcc475e66a521c2880cbb29e
SNRainiar <49141130+SamuNatsu@users.noreply.github.com>
2022-06-13 10:29:19 +0800

    Update and rename Dicnic.cpp to Dinic.cpp

--- Graph theory/Dinic.cpp
+++ Graph theory/Dinic.cpp
@@ -1,14 +1,15 @@
 // Dicnic算法本质上是优化的EK算法 O(V^2E)，适合稠密图
 // Author：SNRainiar, from SZTU_AtDawn
 
-#define MAX_N 10000
+#define MAX_N 100
+#define MAX_M 10000
 #define INF 0x3f3f3f3f
 
 // 流量网络
-int head[];
+int head[MAX_N];
 struct e {
     int to, f, nxt;
-} edge[];
+} edge[MAX_M];
 
 // 深度、当前弧
 int dep[MAX_N], cur[MAX_N];
@@ -23,30 +24,25 @@ bool bfs(int s, int t) {
         int u = que.front();
         que.pop();
         for (int i = head[u]; i; i = edge[i].nxt)
-            if (!dep[edge[i].to] && edge[i].f > 0) {
+            if (edge[i].f && !dep[edge[i].to]) {
                 dep[edge[i].to] = dep[u] + 1;
                 que.emplace(edge[i].to);
             }
     }
-    return dep[t] > 0;
+    return dep[t];
 }
 // 修改流量，使用当前弧优化
 int dfs(int u, int t, int f) {
     if (!f || u == t)
         return f;
     int flow = 0;
-    for (int i = cur[u]; i; i = edge[i].nxt) {
+    for (int i = cur[u]; flow < f && i; i = edge[i].nxt) {
         cur[u] = i;
-        if (dep[edge[i].to] == dep[u] + 1 && edge[i].f > 0) {
-            int tmp = dfs(edge[i].to, t, std::min(f, edge[i].f));
-            if (!tmp)
-                continue;
+        if (edge[i].f && dep[edge[i].to] == dep[u] + 1) {
+            int tmp = dfs(edge[i].to, t, std::min(f - flow, edge[i].f));
             edge[i].f -= tmp;
             edge[i ^ 1].f += tmp;
             flow += tmp;
-            f -= tmp;
-            if (!f)
-                break;
         }
     }
     return flow;
