6ba3a943ec3c93131f60d6017bcacf4522bb36b5
SNRainiar <49141130+SamuNatsu@users.noreply.github.com>
2022-06-13 08:46:01 +0800

    Update SPFA_EK.cpp

--- Graph theory/SPFA_EK.cpp
+++ Graph theory/SPFA_EK.cpp
@@ -1,4 +1,4 @@
-// 最小费用最大流
+// 基于SPFA-EK的最小费用最大流
 // Author：SNRainiar, from SZTU_AtDawn
 
 #define MAX_N 1000
@@ -39,7 +39,7 @@ bool spfa(int st, int ed) {
     }
     return dis[ed] != 0x3f3f3f3f;
 }                
-// 最小费用最大流主函数
+// 最小费用最大流主函数 O(|V|^2|E|^3)，实际情况远小于理论时间复杂度
 std::pair<int, int> mcmf(int st, int ed) {
     int cost = 0, flow = 0;
     while (spfa(st, ed)) {
