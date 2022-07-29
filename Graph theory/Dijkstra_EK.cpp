9319f071fe24d68d467694fc309a792d42a0ecdf
SNRainiar <49141130+SamuNatsu@users.noreply.github.com>
2022-07-29 15:42:34 +0800

    Update Dijkstra_EK.cpp

--- Graph theory/Dijkstra_EK.cpp
+++ Graph theory/Dijkstra_EK.cpp
@@ -15,13 +15,35 @@ struct e {
 int dis[MAX_N], prv[MAX_N], pre[MAX_N], h[MAX_N];
 // 集合存在标志
 bool vis[MAX_N];
+// 势函数初始化（仅当原始费用网络中存在有容量的负权边才需要调用，如计算最大费用最大流）
+void SFPA(int s) {
+    std::queue<int> que;
+    que.emplace(s);
+    memset(h, 0x3f, sizeof(h));
+    h[s] = 0;
+    memset(vis, false, sizeof(vis));
+    vis[s] = true;
+    while (!que.empty()) {
+        int u = que.front();
+        que.pop();
+        vis[u] = false;
+        for (int i = head[u]; i; i = edge[i].nxt)
+            if (edge[i].f && h[edge[i].to] > h[u] + edge[i].c) {
+                h[edge[i].to] = h[u] + edge[i].c;
+                if (!vis[edge[i].to]) {
+                    vis[edge[i].to] = true;
+                    que.emplace(edge[i].to);
+                }
+            }
+    }
+}
 // 寻找最小费用増广路（Dijkstra）
 bool dijkstra(int s, int t) {
-    memset(dis, 0x3f, sizeof(dis));
-    memset(vis, false, sizeof(vis));
     std::priority_queue<std::pair<int, int>> que;
-    dis[s] = 0;
     que.emplace(0, s);
+    memset(dis, 0x3f, sizeof(dis));
+    dis[s] = 0;
+    memset(vis, false, sizeof(vis));
     while (!que.empty()) {
         auto [d, u] = que.top();
         que.pop();
@@ -50,7 +72,7 @@ std::pair<int, int> mcmf(int s, int t) {
             mn = std::min(mn, edge[pre[i]].f);
         flow += mn;
         // 需要使用势函数更新
-        cost += mn * h[ed];
+        cost += mn * h[t];
         for (int i = t; i != s; i = prv[i]) {
             edge[pre[i]].f -= mn;
             // 反流边
