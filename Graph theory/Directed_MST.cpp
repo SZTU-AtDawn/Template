d3a08634e4c6c73cafbf05f45f25d88ba499ff25
Cold_Snapdragon <55649366+ColdSnapdragon@users.noreply.github.com>
2022-08-02 13:40:03 +0800

    Update Directed_MST.cpp

--- Graph theory/Directed_MST.cpp
+++ Graph theory/Directed_MST.cpp
@@ -1,3 +1,5 @@
+//有向图的最小生成树————朱刘算法
+//算法顺序：扫描前驱——搜索有向环——标记环——缩点
 #include<bits/stdc++.h>
 using namespace std;
 const int maxn=1e3+5,inf=1e9;
@@ -11,13 +13,13 @@ void mst(int u)
 {
     ans=0;
     for(int i=1;i<=n;++i)
-        tag[i]=0;
-    while(1)//能不能把根u也打tag? 答：不能
+        tag[i]=0;  //tag[x]为true表示点x被移除，不再参加任何步骤
+    while(1)
     {
         for(int i=1;i<=n;++i)
         {
             if(tag[i] || i==u) continue;
-            pre[i]=i; w[i]=inf;
+            pre[i]=i; w[i]=inf; //pre[i],w[i]记录点i的最小入边以及边权
             for(int j=1;j<=n;++j)
             {
                 if(!tag[j])
@@ -27,12 +29,14 @@ void mst(int u)
                         pre[i]=j;
                     }
             }
-            if(pre[i]==i)
+            if(pre[i]==i) //如果找不到入边，则树形图不存在
             {
                 ans=-1;
                 return;
             }
         }
+        /*判断一个点是否在环上？首先，如果沿着前驱能找到根，那一定不在环上；若陷入循环（设一个计数器），却又回不到自己，说明点在水母触手上；
+        否则，点必在一个环上。可以观察每个点最后是因为什么退出循环的。如果所有点都没有检测到在环上，则统计结果输出。*/
         int t;
         for(t=1;t<=n;++t)
         {
@@ -43,10 +47,10 @@ void mst(int u)
                 j=pre[j];
                 ++cnt;
             }
-            if(j==u || cnt>n) continue;
+            if(j==u || cnt>n) continue; //t不在环上
             else break;
         }
-        if(t>n)
+        if(t>n) //如果找不到环，统计结果并返回
         {
             for(int i=1;i<=n;++i)
             {
@@ -57,11 +61,12 @@ void mst(int u)
         }
         for(int i=1;i<=n;++i)
             vis[i]=0;
+        //将t所在的环缩至点t
         int j=t;
         do{
             ans+=w[j];
             j=pre[j];
-            vis[j]=tag[j]=1; //vis标记当前环，tag标记所有环
+            vis[j]=tag[j]=1; //vis标记当前环（每次重置为0），tag标记所有环
         }while(j!=t);
         tag[t]=0;
         for(int k=1;k<=n;++k)//环外点k
@@ -93,7 +98,7 @@ int main()
         {
             int a,b;
             scanf("%d%d",&a,&b);
-            G[a][b]=sqrt((pos[a].x-pos[b].x)*(pos[a].x-pos[b].x)+(pos[a].y-pos[b].y)*(pos[a].y-pos[b].y));
+            G[a][b]=sqrt((pos[a].x-pos[b].x)*(pos[a].x-pos[b].x)+(pos[a].y-pos[b].y)*(pos[a].y-pos[b].y)); //这里的边权为两点距离
         }
         mst(1);
         if(ans==-1)
