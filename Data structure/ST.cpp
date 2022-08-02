49a56e291d635b31f1e5888e33a6c46a9ecee302
SNRainiar <49141130+SamuNatsu@users.noreply.github.com>
2022-08-02 10:54:37 +0800

    Update ST.cpp

--- Data structure/ST.cpp
+++ Data structure/ST.cpp
@@ -16,20 +16,20 @@ void init() {
 
 // 构造 O(nlogn)
 void build(int a[]) {
-	for (int i = 1; i <= MAX_N; ++i)
-		st[i][0] = a[i];
-	for (int j = 1; j <= MAX_N_LOG; ++j)
-		for (int i = 1, lmt = MAX_N + 1 - (1 << j); i <= lmt; ++i)
-			// 合并左半边和右半边结果（多种维护方案只需要满足倍增可合并性质即可）
-			st[i][j] = std::max(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);       // 最大值维护
+    for (int i = 1; i <= MAX_N; ++i)
+        st[i][0] = a[i];
+    for (int j = 1; j <= MAX_N_LOG; ++j)
+        for (int i = 1, lmt = MAX_N + 1 - (1 << j); i <= lmt; ++i)
+           // 合并左半边和右半边结果（多种维护方案只需要满足倍增可合并性质即可）
+            st[i][j] = std::max(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);       // 最大值维护
             // st[i][j] = std::min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);    // 最小值维护
 }
 
 // 查询最值 O(1)
 inline int query(int l, int r) {
-	// 求区间长度的log2对数值
-	int k = lg2[r - l + 1];
-	// 拆分区间分别取值，再合并答案
-	return std::max(st[l][k], st[r - (1 << k) + 1][k]);     // 最大值查询
+    // 求区间长度的log2对数值
+    int k = lg2[r - l + 1];
+    // 拆分区间分别取值，再合并答案
+    return std::max(st[l][k], st[r - (1 << k) + 1][k]);     // 最大值查询
     // return std::min(st[l][k], st[r - (1 << k) + 1][k]);  // 最小值查询
 }
