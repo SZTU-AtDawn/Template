f763f7f992a5af348df9297d294e0943fb852b79
Cold_Snapdragon <55649366+ColdSnapdragon@users.noreply.github.com>
2022-09-12 21:30:08 +0800

    Update SCC_Shrink_Point.cpp

--- Graph theory/SCC_Shrink_Point.cpp
+++ Graph theory/SCC_Shrink_Point.cpp
@@ -1,22 +1,17 @@
 //强连通分量缩点
-#include<bits/stdc++.h> //洛谷——【模板】缩点（求有向图中点权和最大的非简单路径）
-#define FLAG cout<<"done"<<endl;
-using namespace std;
-const int maxn=1e4+5,M=1e5+5;
-const double inf=1061109567;
-const double eps=1e-4;
-//链式前向星存原图，邻接表存缩点图
+
+//链式前向星存原图，邻接表存缩点图（DAG）
 vector<int> G[maxn];
 int to[M],hd[maxn],nt[M],e=1;
 int dfn[maxn],low[maxn],tim;
 int id[maxn],in[maxn];
 int ar[maxn],tag[maxn],st[maxn],up;
-inline void debug(initializer_list<long long> il){for(const auto &i:il)cout<<i<<" ";cout<<"\n";}
+
 inline void add(int x,int y){to[e]=y;nt[e]=hd[x];hd[x]=e++;}
 void tarjan(int x) //SCC-tarjan的参数只有当前节点x
 {
     dfn[x]=low[x]=++tim;
-    tag[x]=1;
+    tag[x]=1;//在栈中
     st[++up]=x;
     for(int k=hd[x];k;k=nt[k])
     {   
@@ -41,25 +36,12 @@ void tarjan(int x) //SCC-tarjan的参数只有当前节点x
         ar[x]=res;
     }
 }
-int mem[maxn];
-int dfs(int x)
-{
-    if(mem[x])
-        return mem[x];
-    for(int i:G[x])
-    {
-        mem[x]=max(mem[x],dfs(i));
-    }
-    mem[x]+=ar[x];
-    return mem[x];
-}
+
 int main()
 {
     cin.sync_with_stdio(false);
     int n,m;
     cin>>n>>m;
-    for(int i=1;i<=n;++i)
-        cin>>ar[i];
     for(int i=1;i<=m;++i)
     {
         int x,y;
@@ -83,10 +65,5 @@ int main()
             }
         }
     }
-    int ans=0;
-    for(int i=1;i<=n;++i)
-        if(in[i]==0)
-            ans=max(ans,dfs(i));
-    cout<<ans;
 }
 //对于一个原图点，如果id[x]!=x，说明它不在缩点图上
